d761b5a8dd6ff4f6b66e21f82adfb1032c1bdf02
Erick Costa <erickcosta2509@gmail.com>
2021-12-30 16:40:30 -0300

    componente tabela #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+
+  const clients = [
+    new Client('Ana', 34, '1'),
+    new Client('Bia', 21, '2'),
+    new Client('Carlos', 23, '3'),
+    new Client('Pedro', 54, '4')
+  ]
+
   return (
     <div className={`
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-500 to-blue-600 text-white
     `}>
       <Layout title="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   )
