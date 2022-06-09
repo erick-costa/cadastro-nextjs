a6a8c44769cbc501c5f9556caf6e7ab1ded5a23d
Erick Costa do Nascimento <erickcosta2509@gmail.com>
2022-06-09 16:26:37 -0300

    componente botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,43 @@
-import Layout from "../components/Layout";
-import Table from "../components/Table";
-import Client from "../core/Client";
+import Button from "../components/Button"
+import Layout from "../components/Layout"
+import Table from "../components/Table"
+import Client from "../core/Client"
 
 export default function Home() {
+  const clients = [
+    new Client("Ana", 34, "1"),
+    new Client("Bia", 21, "2"),
+    new Client("Carlos", 23, "3"),
+    new Client("Pedro", 54, "4"),
+  ]
 
-    const clients = [
-    new Client('Ana', 34, '1'),
-    new Client('Bia', 21, '2'),
-    new Client('Carlos', 23, '3'),
-    new Client('Pedro', 54, '4')
-    ]
+  function selectedClient(client: Client) {
+    console.log(client.name)
+  }
 
-    function selectedClient(client: Client) {
-        console.log(client.name)
-    }
+  function deletedClient(client: Client) {
+    console.log(`Excluir... ${client.name}`)
+  }
 
-    function deletedClient(client: Client) {
-        console.log(`Excluir... ${client.name}`)
-    }
-
-    return (
-    <div className={`
+  return (
+    <div
+      className={`
         flex h-screen justify-center items-center
         bg-gradient-to-r from-purple-500 to-blue-600 text-white
-    `}>
-        <Layout title="Cadastro Simples">
-        <Table clients={clients} selectedClient={selectedClient} deletedClient={deletedClient}></Table>
-        </Layout>
+    `}
+    >
+      <Layout title="Cadastro Simples">
+        <div className="flex justify-end">
+          <Button color="blue" className="mb-4">
+            Novo Cliente
+          </Button>
+        </div>
+        <Table
+          clients={clients}
+          selectedClient={selectedClient}
+          deletedClient={deletedClient}
+        ></Table>
+      </Layout>
     </div>
-    )
+  )
 }
